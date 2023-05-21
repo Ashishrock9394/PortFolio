5b673353b10b1eaf8e9277aa7418937b26f7f659
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-05-21 08:42:52 +0530

    check Signed-off-by: Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 //typed js
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Web Developer', 'Frontend Developer', 'Node.js Developer'],
+    strings: ['Web Developer', 'Database Manager', 'Frontend Developer', 'Node.js Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
